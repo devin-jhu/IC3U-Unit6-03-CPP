7e313de37c239ff8a8cad97f0fdd4181512c1330
Devin Jhu <devin.jhu@mths.ca>
2022-06-05 21:34:51 +0000

    code

--- numbers.cpp
+++ numbers.cpp
@@ -2,24 +2,43 @@
 //
 // Created by Devin Jhu
 // Created on May 2022
-// The loop adder
+// The celcius to fahrenheit calculator
 
 #include <iostream>
 #include <random>
+#include <array>
+
+
+int smallest(std::array<int, 10> numberArray) {
+    // this function finds the highest number in the num_array
+    int smallestNumber = numberArray[0];
+
+    for (int counter = 0; counter < numberArray.size(); counter++) {
+        if (numberArray[counter] < smallestNumber) {
+            smallestNumber = numberArray[counter];
+        }
+    }
+
+    return smallestNumber;
+}
+
 
 int main() {
-    // this function finds the average of 10 random numbers
+    // this function generates 10 random numbers, calls a function,
+    // and outputs the highest one
 
-    int numberArray[10];
+    std::array<int, 10> numberArray;
     int randomNumber;
-    float average;
+    int smallestNumber;
 
     // random seed
     std::random_device rseed;
     std::mt19937 rgen(rseed());  // mersenne twister
     std::uniform_int_distribution<int> idist(0, 100);  // random from 0 - 100
 
-    std::cout << "The random number average finder" << std::endl;
+    std::cout <<
+    "This program generates 10 random numbers and finds the lowest one.\n"
+    << std::endl;
 
     // process and output
     for (int counter = 0; counter < 10; counter++) {
@@ -28,11 +47,11 @@ int main() {
         std::cout << "Random number: " << randomNumber << std::endl;
     }
 
-    average = std::accumulate(std::begin(numberArray), std::end(numberArray),
-        0, std::plus<int>());
-    average /= 10;
+    smallestNumber = smallest(numberArray);
 
-    std::cout << "\nThe average is " << average << "." << std::endl;
+    // output
+    std::cout << "The smallest number is " << smallestNumber << "."
+    << std::endl;
 
     std::cout << "\nDone." << std::endl;
 }
